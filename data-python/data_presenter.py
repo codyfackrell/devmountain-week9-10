3eb1903a38e493e626717dc6083df3330a84fce2
Cody Fackrell <codyfackrell56@gmail.com>
2023-03-20 21:48:23 -0600

    python completed

---
+++ data-python/data_presenter.py
@@ -0,0 +1,28 @@
+open_file = open('CupcakeInvoices.csv')
+
+# for row in open_file:
+#     print(row)
+
+# for row in open_file:
+#     row = row.strip()
+#     values = row.split(',')
+#     print(values[2])
+
+# for row in open_file:
+#     row = row.strip()
+#     values = row.split(',')
+#     total = (int(values[3])) * (float(values[4]))
+#     print("{0:.2f}".format(total))
+
+
+# total = 0 
+
+# for row in open_file:
+#     row = row.strip()
+#     values = row.split(',')
+#     total = total + (int(values[3]) * float(values[4]))
+    
+
+# print("{0:.2f}".format(total))
+
+open_file.close()
